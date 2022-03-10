e1cc407c5fbfbed40f6941ad6022dd009614eeb8
Esrafil <57289402+EsrafilElahi@users.noreply.github.com>
2022-03-11 00:45:32 +0330

    init app and add custom fonts

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+
+const customFonts = {
+  yekan: require("./assets/fonts/yekan.ttf"),
+  oswald: require("./assets/fonts/Oswald-Light.ttf"),
+  oswald_regular: require("./assets/fonts/Oswald-Regular.ttf"),
+};
 
 export default function App() {
+  const [isFontLoaded] = useFonts(customFonts);
+
+  if (!isFontLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.txt}>Hello World</Text>
     </View>
   );
 }
@@ -13,8 +25,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  txt: {
+    fontFamily: "oswald",
   },
 });
